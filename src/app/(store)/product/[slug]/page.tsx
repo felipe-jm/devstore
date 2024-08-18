6aa72ea3fb41ede0e3af45691162ff094e7a3003
Felipe Jung <felipemattoseu@gmail.com>
2024-08-18 09:28:22 -0400

    Add SSG to featured products

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -31,6 +31,16 @@ export async function generateMetadata({
   };
 }
 
+export async function generateStaticParams() {
+  const response = await api("/products/featured");
+
+  const products = await response.json();
+
+  return products.map((product: Product) => ({
+    slug: product.slug,
+  }));
+}
+
 export default async function ProductPage({ params }: ProductProps) {
   const product = await getProduct(params.slug);
 
